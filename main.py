75b65008bc9f75bed26ce8767a555dcbaa7b45fa
Fabrizio <fabrizio.tappero@gmail.com>
2022-02-06 15:25:36 +0100

    renamed stuff

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import eel
 
 eel.init('web')
+eel.start('index.html',mode='chrome-app', size=(300, 200), block=False)
 
+# list of Python functions that GUI can use to send stuff
 @eel.expose
-def js_says(x):
-    print('Js says',x)
-
-eel.start('index.html',mode='chrome-app', size=(300, 200), block=False)  # Start
+def talk_to_py(x):
+    print('Receiving:', x)
 
 t=0
 while True:
     eel.sleep(1)
-    eel.py_says(t)
+    eel.talk_to_gui(t) # function defined in index.html
     t += 1
